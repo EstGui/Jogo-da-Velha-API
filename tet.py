22218d08e5aebd162a774db313e7db0a156f4406
Guilherme Esteves <guilherme.o.esteves@gmail.com>
2024-08-08 15:58:51 -0300

    code correction

--- tet.py
+++ tet.py
@@ -2,15 +2,11 @@ import copy
 from random import choice
 
 
-board = [[' ']*3 for _ in range(3)]
-
-
 def genCordinates(num):
     row = (num - 1) // 3
     column = (num - 1) % 3
 
     return row, column
-
 
 def genIndex(tuple):
     return tuple[0] * 3 + tuple[1]
@@ -20,7 +16,7 @@ def utility(state):
     if all([cell == '' for row in state for cell in row]):
         ini_opts = [[(0, 0), ''], [(0, 2), ''], [(2, 0), ''], [(2, 2), '']]
         return choice(ini_opts)
-
+    
     actions = []
     possibilities = act(state)
 
@@ -52,21 +48,22 @@ def utility(state):
         for a in actions:
             if a[1] < menor[1]:
                 menor = a
+        # menor = min([a[1] for a in actions])
 
         return (menor[0], menor[1], ([x[1] for x in actions].count(1) / len(actions)) * 100)
     
 
 def get_state_value(state):
     for row in state:
-        if all([cell == row[0] and cell != ' ' for cell in row]):
+        if all([cell == row[0] and cell != '' for cell in row]):
             return 1 if row[0] == 'X' else -1 
 
     for col in range(3):
-        if all([state[row][col] == state[0][col] and state[0][col] != ' ' for row in range(3)]):
+        if all([state[row][col] == state[0][col] and state[0][col] != '' for row in range(3)]):
             return 1 if state[0][col] == 'X' else -1
 
-    if (state[0][0] == state[1][1] == state[2][2] and state[0][0] != ' ') or \
-       (state[0][2] == state[1][1] == state[2][0] and state[1][1] != ' '):
+    if (state[0][0] == state[1][1] == state[2][2] and state[0][0] != '') or \
+       (state[0][2] == state[1][1] == state[2][0] and state[1][1] != ''):
         return 1 if state[1][1] == 'X' else -1
         
     return 0
@@ -74,18 +71,18 @@ def get_state_value(state):
 
 def terminal(board):
     for row in board:
-        if all([cell == row[0] and cell != ' ' for cell in row]):
+        if all([cell == row[0] and cell != '' for cell in row]):
             return True
 
     for col in range(3):
-        if all([board[row][col] == board[0][col] and board[0][col] != ' ' for row in range(3)]):
+        if all([board[row][col] == board[0][col] and board[0][col] != '' for row in range(3)]):
             return True
 
-    if (board[0][0] == board[1][1] == board[2][2] and board[0][0] != ' ') or \
-       (board[0][2] == board[1][1] == board[2][0] and board[1][1] != ' '):
+    if (board[0][0] == board[1][1] == board[2][2] and board[0][0] != '') or \
+       (board[0][2] == board[1][1] == board[2][0] and board[1][1] != ''):
         return True
         
-    if all([cell != ' ' for row in board for cell in row]):
+    if all([cell != '' for row in board for cell in row]):
         return True
 
     return False
@@ -102,7 +99,7 @@ def player(board):
 
 
 def act(board):
-    return [(row, col) for row in range(3) for col in range(3) if board[row][col] == ' ']
+    return [(row, col) for row in range(3) for col in range(3) if board[row][col] == '']
 
 
 def result(state, action):
